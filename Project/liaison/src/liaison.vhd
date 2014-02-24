fd6cdb47356bb267d17a1a4aa4553658df57a6ef
Rune Holmgren <Raane.Holm@gmail.com>
2014-02-24 15:24:39 +0100

    Add the entities to liaison

--- Project/liaison/src/liaison.vhd
+++ Project/liaison/src/liaison.vhd
@@ -14,5 +14,40 @@ end liaison;
 
 architecture liaison of liaison is
 begin
+	onebitvoter: entity work.onebitvoter
+	port map(
+	clk => clk,
+	reset => reset,
+	md_data => md_data,
+	voted_data => voted_data_bit,
+	status => status
+	);
+	
+	controller: entity work.controller
+		port map(
+			clk => clk,
+			reset => reset,
+			di_ready => di_ready,
+			controll_signals
+		);
+		
+	registers: entity work.registers
+		port map(
+		clk => clk,
+		reset => reset,
+		voted_data_bit => voted_data_bit,
+		status => status,
+		controll_signals => controll_signals,
+		ECC => ECC,
+		voted_data_out => voted_data_out,
+		status_out => status_out
+		);
+		
+	ECC: entity	work.ECC
+		port map(
+		voted_data_out => voted_data_out,
+		status_out => status_out,
+		ECC => ECC
+		);
 
 end liaison;
