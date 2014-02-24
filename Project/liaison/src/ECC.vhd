1795c742126064bf9591a4a94790f227fd16ba3a
Rune Holmgren <Raane.Holm@gmail.com>
2014-02-24 16:44:51 +0100

    Adding some aliases and a process to the ecc

--- Project/liaison/src/ECC.vhd
+++ Project/liaison/src/ECC.vhd
@@ -10,6 +10,23 @@ entity ECC is
 end ECC;
 
 architecture ECC of ECC is
+
+alias a is voted_data_out(0);
+alias b is voted_data_out(1);
+alias c is voted_data_out(2);
+alias d is voted_data_out(3);
+alias e is voted_data_out(4);
+alias f is voted_data_out(5);
+alias g is voted_data_out(6);
+alias h is voted_data_out(7);
+alias i is status_out(0);
+alias j is status_out(1);
+alias k is status_out(2);
+
 begin
+
+	process (a,b,c,d,e,f,g,h,i,j,k)
+	begin
+	end process;
 	
 end ECC;
