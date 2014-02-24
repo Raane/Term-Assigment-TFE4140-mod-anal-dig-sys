75eef7a33c9fbe531fb8755cdfe64507654923fb
Sygnious <skienstronder@gmail.com>
2014-02-24 16:48:06 +0100

    Pushing random project crap

--- Project/liaison/src/controller.vhd
+++ Project/liaison/src/controller.vhd
@@ -13,10 +13,47 @@ entity controller is
 end controller;
 
 architecture controller of controller is
-	-- Counter is used to control when to activate do_ready
-	signal counter: integer range 0 to 50; 
+	-- Interwall for di_ready and do_ready: 19 clock cycles
+	-- (8 voted data, 3 status data, 8 parity bits)
+
+	-- Counter is used to control outputs, 
+	-- like when to activate do_ready, 
+	-- selecting register cells for storing inputs,
+	-- selecting which cell to give current output etc.
+	constant counter_end: integer := 50;
+	signal counter: integer range 0 to counter_end; 
 begin
 	
-	-- Reset sets all to 0
-
+	
+	clock_tick : process(clk)
+	begin
+	if (rising_edge(clk)) then
+		-- Reset sets all to 0
+		if (reset='1') then 
+			control_signals <= "0000000000";
+			voted_data_selector <= "000";
+			do_ready <= '0';
+			counter <= 0;
+		else
+			-- di_ready is set to 1 (usually when counter is 0)
+			-- initializes counter and sets first register for saving
+			if (di_ready == '1') then
+				counter <= 1;
+			-- 
+			elsif (
+				
+		end if;
+	end if;
+	
+	end process;
+	
+	set_input_read: process(counter)
+	begin
+		case counter is
+			when 
+		
+		
+	end process;
+	
+	
 end controller;
