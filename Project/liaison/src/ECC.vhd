bfa45751ec96592d3adfd51bbb5ef23a6d8cc4b3
Raane <Raane.Holm@gmail.com>
2014-03-06 14:56:25 +0100

    Add the hamming module

--- Project/liaison/src/ECC.vhd
+++ Project/liaison/src/ECC.vhd
@@ -16,10 +16,17 @@ alias a is voted_data_out(0);
 alias b is voted_data_out(1);
 alias c is voted_data_out(2);
 alias d is voted_data_out(3);
-alias e is status_out(0);
-alias f is status_out(1);
-alias g is status_out(2);
+alias e is voted_data_out(4);
+alias f is voted_data_out(5);
+alias g is voted_data_out(6);
+alias h is voted_data_out(7);
+alias i is status_out(0);
+alias j is status_out(1);
+alias k is status_out(2);
 
 begin
-
+		ECC_signal(0) <= a xor b xor d xor e xor g xor i xor k; --parity1
+		ECC_signal(1) <= a xor c xor d xor f xor g xor j xor k; --parity2
+		ECC_signal(2) <= b xor c xor d xor h xor i xor j xor k; --parity3
+		ECC_signal(3) <= e xor f xor g xor h xor i xor j xor k; --parity4
 end ECC;
